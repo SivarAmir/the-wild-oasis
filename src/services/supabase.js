e35304abd6f454047222c9597af5241d9617c02a
Sivar <sevaramer999@gmail.com>
2023-11-10 16:44:37 +0300

    add cabins and deleting

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://lhkuboufqxrqeifinwrc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxoa3Vib3VmcXhycWVpZmlud3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyOTc1NTQsImV4cCI6MjAxNDg3MzU1NH0.0yU8AB_aeforOErXlTow1fg0fkvWmnO_5nYhQCiLZrI";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
